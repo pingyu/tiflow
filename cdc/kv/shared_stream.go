4b7c9b4302ed878fef5f50d7f594ac8ab950bd87
Ping Yu <yuping@pingcap.com>
2025-11-04 12:12:16 +0800

    hack
    Signed-off-by: Ping Yu <yuping@pingcap.com>

--- cdc/kv/shared_stream.go
+++ cdc/kv/shared_stream.go
@@ -234,9 +234,9 @@ func (s *requestedStream) receive(
 				zap.String("addr", rs.storeAddr),
 				zap.String("code", grpcstatus.Code(err).String()),
 				zap.Error(err))
-			if sharedconn.StatusIsEOF(grpcstatus.Convert(err)) {
-				return nil
-			}
+			// if sharedconn.StatusIsEOF(grpcstatus.Convert(err)) {
+			// 	return nil
+			// }
 			return errors.Trace(err)
 		}
 		if len(cevent.Events) > 0 {
